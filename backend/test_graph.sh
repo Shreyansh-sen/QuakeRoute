6da86dab7f4b0c9c2f48551e0ca0f6c88be084a6
pritam <pritam.p@appsforbharat.com>
2026-05-23 16:05:58 +0530

    removed unwanted files

--- backend/test_graph.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -s -X POST "http://localhost:8000/api/v1/graph/build" \
-  -H "Content-Type: application/json" \
-  -d '{"disaster_ids":[1,2],"resource_ids":null,"include_route_geometry":true,"max_distance_km":50}' \
-  -o /tmp/graph_result.json
-
-python3 /Users/pritam/Quantumx/QuakeRoute/backend/test_graph.py
-
